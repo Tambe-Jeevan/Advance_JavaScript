8afad0d6708503d251e398881d78049d49e34de9
Jeevan Tambe <tambejeevan816@gmail.com>
2025-08-06 18:03:43 +0530

    Add javascript Real Time Watch Project.

---
+++ 6_Real_Time_Watch/script.js
@@ -0,0 +1,7 @@
+let time = document.querySelector('#time');
+
+setInterval(function (){
+    let date = new Date();
+    // console.log(date.toLocaleTimeString());
+    time.innerHTML = date.toLocaleTimeString();
+},1000)
